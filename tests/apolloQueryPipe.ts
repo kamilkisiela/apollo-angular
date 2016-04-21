2b3b4939469df9324debabfefd553430e02aa581
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2016-04-21 22:57:51 +0200

    feat(ApolloQueryPipe): first property of data by default

--- tests/apolloQueryPipe.ts
+++ tests/apolloQueryPipe.ts
@@ -7,14 +7,35 @@ describe('ApolloQueryPipe', () => {
     pipe = new ApolloQueryPipe();
   });
 
-  it('should capitalize all words in a string', () => {
-    const object = {
-      data: {
-        foo: 'bar',
-      },
-    };
-    const result = pipe.transform(object, 'foo');
+  it('should return nothing if first argument is not an object', () => {
+    expect(pipe.transform('test')).toBeUndefined();
+  });
 
-    expect(result).toEqual(object.data.foo);
+  it('should return nothing on empty object', () => {
+    expect(pipe.transform({})).toBeUndefined();
+  });
+
+  it('should return nothing on empty data', () => {
+    expect(pipe.transform({
+      data: {},
+    })).toBeUndefined();
+  });
+
+  it('should use the first property of data if name is not defined', () => {
+    expect(pipe.transform({
+      data: {
+        foo: 'foo',
+        bar: 'bar',
+      },
+    })).toEqual('foo');
+  });
+
+  it('should use a property where key matches name', () => {
+    expect(pipe.transform({
+      data: {
+        foo: 'foo',
+        bar: 'bar',
+      },
+    }, 'bar')).toEqual('bar');
   });
 });
