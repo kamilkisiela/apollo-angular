80173514d01708dc0c4482bfdf129723f5872104
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2016-05-25 23:24:15 +0200

    feat(ApolloQueryPipe): Make it compatible with NG RC1

--- src/apolloQueryPipe.ts
+++ src/apolloQueryPipe.ts
@@ -1,14 +1,14 @@
 /// <reference path="../typings/main.d.ts" />
 
-import {Pipe} from '@angular/core';
+import {
+  Pipe,
+} from '@angular/core';
 
 @Pipe({
   name: 'apolloQuery',
 })
 export class ApolloQueryPipe {
-  public transform(obj, args) {
-    const name = args[0] || '';
-
+  public transform(obj: any, name: string = '') {
     if (obj && obj.data && name !== '' && obj.data[name]) {
       return obj.data[name];
     }
