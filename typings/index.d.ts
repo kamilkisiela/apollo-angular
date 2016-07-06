b262b6921ada227b69f80e122eb836610811a5c8
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2016-07-06 21:21:46 +0200

    chore(typings): update and remove old definitions

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,5 +1,5 @@
 /// <reference path="globals/es6-shim/index.d.ts" />
-/// <reference path="globals/graphql/index.d.ts" />
 /// <reference path="globals/isomorphic-fetch/index.d.ts" />
 /// <reference path="globals/jasmine/index.d.ts" />
+/// <reference path="modules/graphql/index.d.ts" />
 /// <reference path="modules/lodash/index.d.ts" />
