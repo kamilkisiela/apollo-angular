fde17447e200d24054a3b143da8d20a251c906fc
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2016-07-16 10:56:37 +0200

    chore: ignore typings

--- typings/index.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference path="globals/es6-shim/index.d.ts" />
-/// <reference path="globals/isomorphic-fetch/index.d.ts" />
-/// <reference path="globals/jasmine/index.d.ts" />
-/// <reference path="modules/graphql/index.d.ts" />
-/// <reference path="modules/lodash/index.d.ts" />
