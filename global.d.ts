31d4aa5245dfc41456177beea07a3d117f329884
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2016-08-16 10:41:30 +0200

    chore: fix lodash typings

--- global.d.ts
+++ global.d.ts
@@ -1,19 +1,19 @@
 declare module 'lodash.isequal' {
-  import main = require('~lodash/index');
+  import main = require('lodash');
   export = main.isEqual;
 }
 
 declare module 'lodash.forin' {
-  import main = require('~lodash/index');
+  import main = require('lodash');
   export = main.forIn;
 }
 
 declare module 'lodash.assign' {
-  import main = require('~lodash/index');
+  import main = require('lodash');
   export = main.assign;
 }
 
 declare module 'lodash.omit' {
-  import main = require('~lodash/index');
+  import main = require('lodash');
   export = main.omit;
 }
