d11b9bbcb1eb63f8271a6a6fe0be4d77def9d37f
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2016-09-05 22:46:35 -0700

    docs(examples): Add proxy to HelloWorld

--- examples/hello-world/src/app/client.ts
+++ examples/hello-world/src/app/client.ts
@@ -3,7 +3,7 @@ import { createNetworkInterface } from 'apollo-client';
 import ApolloClient from 'apollo-client';
 
 export const client = new ApolloClient({
-  networkInterface: createNetworkInterface('http://localhost:4300/graphql', {
+  networkInterface: createNetworkInterface('/graphql', {
     credentials: 'same-origin',
   }),
 });
