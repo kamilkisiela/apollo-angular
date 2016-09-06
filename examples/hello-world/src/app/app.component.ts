7eb02e6b7f2d495f67179eb9ba78cb8792d30233
Kamil Kisiela <kamilkisiela@users.noreply.github.com>
2016-09-05 23:09:56 -0700

    Move HelloWorld to AngularCLI instead of Meteor (#88)

--- examples/hello-world/src/app/app.component.ts
+++ examples/hello-world/src/app/app.component.ts
@@ -4,27 +4,30 @@ import { Angular2Apollo, ApolloQueryObservable } from 'angular2-apollo';
 import { ApolloQueryResult } from 'apollo-client';
 import { Subject } from 'rxjs/Subject';
 
+// We need this to parse graphql string
 import gql from 'graphql-tag';
 
 import 'rxjs/add/operator/debounceTime';
 import 'rxjs/add/operator/map';
 
-import template from './app.component.html';
-
 @Component({
-  selector: 'app',
-  template
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
 })
 export class AppComponent implements OnInit, AfterViewInit {
+  // Observable with GraphQL result
   public users: ApolloQueryObservable<any>;
   public firstName: string;
   public lastName: string;
   public nameControl = new FormControl();
+  // Observable variable of the graphql query
   public nameFilter: Subject<string> = new Subject<string>();
 
+  // Inject Angular2Apollo service
   constructor(private angular2Apollo: Angular2Apollo) {}
 
   public ngOnInit() {
+    // Query users data with observable variables
     this.users = this.angular2Apollo.watchQuery({
       query: gql`
         query getUsers($name: String) {
@@ -41,18 +44,23 @@ export class AppComponent implements OnInit, AfterViewInit {
       variables: {
         name: this.nameFilter,
       },
-    }).map(result => result.data.users);
+    })
+      // Return only users, not the whole ApolloQueryResult  
+      .map(result => result.data.users) as ApolloQueryObservable<any>;
 
+    // Add debounce time to wait 300 ms for a new change instead of keep hitting the server
     this.nameControl.valueChanges.debounceTime(300).subscribe(name => {
       this.nameFilter.next(name);
     });
   }
 
   public ngAfterViewInit() {
+    // Set nameFilter to null after NgOnInit happend and the view has been initated
     this.nameFilter.next(null);
   }
 
   public newUser(firstName: string) {
+    // Call the mutation called addUser
     this.angular2Apollo.mutate({
       mutation: gql`
         mutation addUser(
--- examples/hello-world/src/app/client.ts
+++ examples/hello-world/src/app/client.ts
@@ -3,5 +3,7 @@ import { createNetworkInterface } from 'apollo-client';
 import ApolloClient from 'apollo-client';
 
 export const client = new ApolloClient({
-  networkInterface: createNetworkInterface('/graphql'),
+  networkInterface: createNetworkInterface('/graphql', {
+    credentials: 'same-origin',
+  }),
 });
