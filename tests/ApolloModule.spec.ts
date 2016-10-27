6d6119ad1a01b16afb81fb5390d684782c38d6ce
Kamil Kisiela <kamil.kisiela@gmail.com>
2016-10-27 23:44:02 +0200

    test(ApolloModule): whole metadata and withClient

---
+++ tests/ApolloModule.spec.ts
@@ -0,0 +1,51 @@
+import { ApolloClient } from 'apollo-client';
+
+import './_common';
+
+import { ApolloModule, SelectPipe, Angular2Apollo } from '../src';
+import { angularApolloClient } from '../src/Angular2Apollo';
+
+describe('ApolloModule', () => {
+  let metadata: any = Reflect.getMetadata('annotations', ApolloModule)[0];
+
+  it('should contain SelectPipe in declarations', () => {
+    expect(include(metadata.declarations, SelectPipe)).toBe(true);
+  });
+
+  it('should export SelectPipe', () => {
+    expect(include(metadata.exports, SelectPipe)).toBe(true);
+  });
+
+  it('should not export Angular2Apollo', () => {
+    expect(include(metadata.exports, SelectPipe)).toBe(true);
+  });
+
+  it('should contain Angular2Apollo in providers', () => {
+    expect(include(metadata.exports, Angular2Apollo)).toBe(false);
+  });
+
+  it('should has withClient method', () => {
+    expect(ApolloModule.withClient).toBeDefined();
+  });
+
+  describe('withClient', () => {
+    const client = {} as ApolloClient;
+    const result = ApolloModule.withClient(client);
+
+    it('should contain ApolloModule as ngModule', () => {
+      expect(result.ngModule).toBe(ApolloModule);
+    });
+
+    it('should contain provider with useValue', () => {
+      expect(result.providers[0]['useValue']).toBe(client);
+    });
+
+    it('should contain provider that provide angularApolloClient', () => {
+      expect(result.providers[0]['provide']).toBe(angularApolloClient);
+    });
+  });
+});
+
+function include(source: any[], find: any): boolean {
+  return source.some(i => i === find);
+}
