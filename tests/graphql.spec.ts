7242a9c4724dbe3b9f1a37e99e014ba2f3a3ce24
Dotan Simha <dotansimha@gmail.com>
2016-11-16 18:28:04 +0200

    chore(tests): added more tests for mutation execution

--- tests/graphql.spec.ts
+++ tests/graphql.spec.ts
@@ -1,4 +1,4 @@
-import 'reflect-metadata';
+import './_common';
 import { Document } from 'graphql';
 import gql from 'graphql-tag';
 import { graphql, wrapPrototype, assignInput, GraphqlInput } from '../src/graphql';
@@ -8,6 +8,14 @@ const query: Document = gql`
   query getBar {
     bar {
       name
+    }
+  }
+`;
+
+const mutation: Document = gql`
+  mutation changeBar {
+      changeBar {
+        name
     }
   }
 `;
@@ -96,21 +104,27 @@ describe('graphql', () => {
   });
 });
 
-describe(`graphql - query`, () => {
+describe(`graphql - query, mutation, subscribe`, () => {
   let spyWatchQuery;
+  let spyMutate;
 
   const mock = {
     watchQuery(options) {
       spyWatchQuery(options);
       return options;
     },
+    mutate(options) {
+      spyMutate(options);
+      return options;
+    },
   };
 
   beforeEach(() => {
     spyWatchQuery = jest.fn();
+    spyMutate = jest.fn();
   });
 
-  const createInstance = (decoratorConfig) => {
+  const createInstance = (decoratorConfig: GraphqlInput[]) => {
     @graphql(decoratorConfig)
     class Foo {
       public ngOnInit: Function;
@@ -124,7 +138,7 @@ describe(`graphql - query`, () => {
     return new Foo(mock);
   };
 
-  it('should execute watchQuery with the correct properties', () => {
+  it('query - should execute watchQuery with the correct properties', () => {
     let input = [{
       query,
     }];
@@ -132,5 +146,65 @@ describe(`graphql - query`, () => {
     foo.ngOnInit();
 
     expect(spyWatchQuery).toBeCalledWith({query});
+  });
+
+  it('mutation - should create execution method on the instance', () => {
+    let foo = createInstance([{
+      name: 'myMutation',
+      mutation,
+    }]);
+
+    foo.ngOnInit();
+
+    expect(foo['myMutation']).toBeDefined();
+    expect(typeof foo['myMutation']).toBe('function');
+  });
+
+  it('mutation - should trigger mutation when executed (without params on call)', () => {
+    let foo = createInstance([{
+      name: 'mutation',
+      mutation,
+    }]);
+
+    foo.ngOnInit();
+    foo['mutation']();
+
+    expect(spyMutate).toBeCalledWith({mutation});
+  });
+
+  const createMutationAndExecuteWith = (executionOptions?: any) => {
+    it('mutation - should trigger mutation when executed (with ' + Object.keys(executionOptions).join(', ') + ')', () => {
+      let foo = createInstance([{
+        name: 'mutation',
+        mutation,
+      }]);
+
+      foo.ngOnInit();
+      foo['mutation'](executionOptions);
+
+      expect(spyMutate).toBeCalledWith(Object.assign({mutation}, executionOptions));
+    });
+  };
+
+  createMutationAndExecuteWith({
+    variables: {
+      test: 1,
+    },
+  });
+
+  createMutationAndExecuteWith({
+    variables: {
+      test: 1,
+    },
+    updateQueries: () => {
+
+    },
+  });
+
+  createMutationAndExecuteWith({
+    optimisticResponse: {
+      __typename: 'changeBar',
+      name: 'Test'
+    },
   });
 });
