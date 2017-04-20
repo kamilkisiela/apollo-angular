b3d75466473032dab9a9983d3d6f69a27765df05
Kamil Kisiela <kamil.kisiela@gmail.com>
2017-04-20 08:35:08 +0200

    test: add mock of Apollo service

--- tests/_mocks.ts
+++ tests/_mocks.ts
@@ -1,5 +1,9 @@
 import { ApolloClient } from 'apollo-client';
 import { mockNetworkInterface } from 'apollo-test-utils';
+import { ReflectiveInjector } from '@angular/core';
+
+import { Apollo, provideClientMap } from '../src/Apollo';
+import { APOLLO_PROVIDERS } from '../src/ApolloModule';
 
 export function mockClient(...args): ApolloClient {
   const networkInterface = mockNetworkInterface(...args);
@@ -9,4 +13,14 @@ export function mockClient(...args): ApolloClient {
     addTypename: false,
     dataIdFromObject: o => o['id'],
   });
+}
+
+export function mockApollo(...args): Apollo {
+  const client = mockClient(...args);
+
+  const injector = ReflectiveInjector.resolveAndCreate([provideClientMap(() => ({
+    default: client,
+  })), APOLLO_PROVIDERS]);
+
+  return injector.get(Apollo);
 }
