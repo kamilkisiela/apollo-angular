96250b6b7052382613dd6bebb31d417a724f6a9e
Kamil Kisiela <kamil.kisiela@gmail.com>
2017-09-21 17:08:38 +0200

    Bring back APOLLO_PROVIDERS and APOLLO_DIRECTIVES

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,27 @@
 export { SelectPipe } from './SelectPipe';
-export { Apollo, ApolloBase, provideClientMap } from './Apollo';
+
 export { ApolloQueryObservable } from './ApolloQueryObservable';
-export { CLIENT_MAP, CLIENT_MAP_WRAPPER } from './tokens';
-export { ClientMap, ClientMapWrapper, ClientWrapper } from './types';
-export { ApolloModule, defaultApolloClient } from './ApolloModule';
+
+export {
+  Apollo,
+  ApolloBase,
+  provideClientMap
+} from './Apollo';
+
+export {
+  CLIENT_MAP,
+  CLIENT_MAP_WRAPPER
+} from './tokens';
+
+export {
+  ClientMap,
+  ClientMapWrapper,
+  ClientWrapper
+} from './types';
+
+export {
+  ApolloModule,
+  defaultApolloClient,
+  APOLLO_PROVIDERS,
+  APOLLO_DIRECTIVES
+} from './ApolloModule';
