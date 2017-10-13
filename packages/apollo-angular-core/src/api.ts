3b2a2085e77d493c39be143bdd958a25c7f2e230
Kamil Kisiela <kamil.kisiela@gmail.com>
2017-10-13 21:26:50 +0200

    feat(core): expose Watcher
    Should have a better name though...

--- packages/apollo-angular-core/src/api.ts
+++ packages/apollo-angular-core/src/api.ts
@@ -1,4 +1,5 @@
 export {Apollo} from './Apollo';
+export {Watcher} from './Watcher';
 export {SelectPipe} from './SelectPipe';
 export {ApolloModule} from './ApolloModule';
 export {ApolloOptions} from './types';
