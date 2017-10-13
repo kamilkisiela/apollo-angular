7c0d3901716fc5642b2501c823836f1c05f58a6a
Kamil Kisiela <kamil.kisiela@gmail.com>
2017-10-13 13:25:27 +0200

    test(core): more tests

---
+++ packages/apollo-angular-core/tests/SelectPipe.spec.ts
@@ -0,0 +1,46 @@
+import './_common';
+
+import { Pipe } from '@angular/core';
+
+import { SelectPipe } from '../src/SelectPipe';
+
+describe('SelectPipe', () => {
+  let pipe;
+  const pipeMetadata: Pipe = Reflect.getMetadata('annotations', SelectPipe)[0];
+
+  beforeEach(() => {
+    pipe = new SelectPipe();
+  });
+
+  test('should return nothing if name is empty', () => {
+    expect(pipe.transform({ foo: 'bar' }, '')).toBe(undefined);
+  });
+
+  test('should return nothing if object is empty', () => {
+    expect(pipe.transform({}, 'foo')).toBe(undefined);
+  });
+
+  test('should return nothing if object is missing', () => {
+    expect(pipe.transform(undefined, 'foo')).toBe(undefined);
+  });
+
+  test('should return nothing if nothing has been found', () => {
+    expect(pipe.transform({ foo: 'bar' }, 'baz')).toBe(undefined);
+  });
+
+  test('should be looking inside data property', () => {
+    const result = {
+      data: { foo: 'bar' },
+    };
+
+    expect(pipe.transform(result, 'foo')).toEqual(result.data.foo);
+  });
+
+  test('should be named select', () => {
+    expect(pipeMetadata.name).toBe('select');
+  });
+
+  test('should be pure', () => {
+    expect(pipeMetadata.pure).toBe(true);
+  });
+});
---
+++ packages/apollo-angular-core/tests/index.spec.ts
@@ -0,0 +1,21 @@
+import './_common';
+
+import * as api from '../src';
+
+import {Apollo} from '../src/Apollo';
+import {SelectPipe} from '../src/SelectPipe';
+import {ApolloModule} from '../src/ApolloModule';
+
+describe('public api', () => {
+  test('should export Apollo', () => {
+    expect(api.Apollo).toBe(Apollo);
+  });
+
+  test('should export ApolloModule', () => {
+    expect(api.ApolloModule).toBe(ApolloModule);
+  });
+
+  test('should export SelectPipe', () => {
+    expect(api.SelectPipe).toBe(SelectPipe);
+  });
+});
