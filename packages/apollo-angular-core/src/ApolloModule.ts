1c77e79614d4d2fb08a6eff054ba99fc16785de6
Kamil Kisiela <kamil.kisiela@gmail.com>
2017-10-13 13:23:13 +0200

    feat(core): put SelectPipe in ApolloModule

--- packages/apollo-angular-core/src/ApolloModule.ts
+++ packages/apollo-angular-core/src/ApolloModule.ts
@@ -1,10 +1,14 @@
 import {NgModule} from '@angular/core';
 
 import {Apollo} from './Apollo';
+import {SelectPipe} from './SelectPipe';
 
 export const PROVIDERS = [Apollo];
+export const DECLARATIONS = [SelectPipe];
 
 @NgModule({
   providers: PROVIDERS,
+  declarations: DECLARATIONS,
+  exports: DECLARATIONS,
 })
 export class ApolloModule {}
