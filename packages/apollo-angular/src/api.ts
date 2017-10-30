0fb24e9958c7738fbb38505950852c115182d09b
Kamil Kisiela <kamil.kisiela@gmail.com>
2017-10-30 19:38:20 +0100

    refactor(core): no longer expose ApolloOptions

--- packages/apollo-angular/src/api.ts
+++ packages/apollo-angular/src/api.ts
@@ -2,4 +2,3 @@ export {Apollo} from './Apollo';
 export {QueryRef} from './QueryRef';
 export {SelectPipe} from './SelectPipe';
 export {ApolloModule} from './ApolloModule';
-export {ApolloOptions} from './types';
