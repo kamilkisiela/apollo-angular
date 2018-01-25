f93c6b80cbb47083e755dfb7ef141bebd7047aa0
Stephen Lautier <stephen.lautier@outlook.com>
2018-01-25 10:59:01 +0100

    feat(api): expose `ApolloBase`

--- packages/apollo-angular/src/api.ts
+++ packages/apollo-angular/src/api.ts
@@ -1,4 +1,4 @@
-export {Apollo} from './Apollo';
+export {Apollo, ApolloBase} from './Apollo';
 export {QueryRef} from './QueryRef';
 export {SelectPipe} from './SelectPipe';
 export {ApolloModule} from './ApolloModule';
