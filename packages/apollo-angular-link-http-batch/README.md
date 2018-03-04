493462fa545247ee1fccbcc7bae876a65507845e
Kamil Kisiela <kamil.kisiela@gmail.com>
2018-03-04 21:59:04 +0100

    feat: http batching link

--- packages/apollo-angular-link-http-batch/README.md
+++ packages/apollo-angular-link-http-batch/README.md
@@ -1,26 +1,29 @@
 # HTTP Link
 
-[![npm version](https://badge.fury.io/js/apollo-angular-link-http.svg)](https://badge.fury.io/js/apollo-angular-link-http)
+[![npm version](https://badge.fury.io/js/apollo-angular-link-http-batch.svg)](https://badge.fury.io/js/apollo-angular-link-http-batch)
 [![Get on Slack](https://img.shields.io/badge/slack-join-orange.svg)](http://www.apollodata.com/#slack)
 
 ## Purpose
 
-An Apollo Link to allow sending a single http request per operation.
+An Apollo Link to combine multiple GraphQL operations into single HTTP request.
 
 ## Installation
 
-`npm install apollo-angular-link-http --save`
+`npm install apollo-angular-link-http-batch --save`
 
 ## Usage
 
 ```ts
-import {HttpLinkModule, HttpLink} from 'apollo-angular-link-http';
+import {
+  HttpBatchLinkModule,
+  HttpBatchLink,
+} from 'apollo-angular-link-http-batch';
 
 @NgModule({
-  imports: [HttpLinkModule],
+  imports: [HttpBatchLinkModule],
 })
 class AppModule {
-  constructor(httpLink: HttpLink) {
+  constructor(httpLink: HttpBatchLink) {
     const link = httpLink.create({uri: '/graphql'});
   }
 }
@@ -33,109 +36,24 @@ present in your application.
 
 ## Options
 
-HTTP Link takes an object with some options on it to customize the behavior of
-the link. If your server supports it, the HTTP link can also send over metadata
-about the request in the extensions field. To enable this, pass
-`includeExtensions` as true. If you would like to use persisted queries or just
-not to send a query, disable `includeQuery`.
+Accepts the same options as `apollo-angular-link-http`.
 
-| name              | value       | default    | required |
-| ----------------- | ----------- | ---------- | -------- |
-| uri               | string      | `/graphql` | false    |
-| includeExtensions | boolean     | `false`    | false    |
-| includeQuery      | boolean     | `true`     | false    |
-| headers           | HttpHeaders | `none`     | false    |
-| withCredentials   | boolean     | `none`     | false    |
-| method            | string      | `POST`     | false    |
+## BatchOptions
+
+The batching options indicate how operations are batched together.
+
+| name          | value    | default | required |
+| ------------- | -------- | ------- | -------- |
+| batchInterval | number   | 10      | false    |
+| batchMax      | number   | 10      | false    |
+| batchKey      | Function | -       | false    |
+
+* `batchInterval` - the maximum time a batch will wait before automatically being sent over the network
+* `batchMax` - the size of batches
+* `batchKey` a function that accepts an operation and returns a string key, which uniquely names the batch the operation belongs to, defaults to returning the same string.
+
+> NOTICE: `batchKey` by default batches together requests with the same uri and the same options. Since options from an operation's context overwrites those from a link you could end up with few differents keys and what it means, few separate requests.
 
 ## Context
 
-The HTTP Link uses the `headers` field on the context to allow passing headers
-to the HTTP request. It also supports the `withCredentials` field for defining
-credentials policy for request. These options will override the same key if
-passed when creating the the link. If some setting is different than the one in
-Options, this one will be used.
-
-| name              | value       | default         | required |
-| ----------------- | ----------- | --------------- | -------- |
-| uri               | string      | `as in options` | false    |
-| includeExtensions | boolean     | `as in options` | false    |
-| includeQuery      | boolean     | `as in options` | false    |
-| headers           | HttpHeaders | none            | false    |
-| withCredentials   | boolean     | `as in options` | false    |
-| method            | string      | `as in options` | false    |
-
-```js
-import {HttpLinkModule, HttpLink} from 'apollo-link-http';
-import {ApolloModule, Apollo} from 'apollo-angular';
-import {InMemoryCache} from 'apollo-cache-inmemory';
-import {HttpClientModule} from '@angular/common/http';
-
-@NgModules({
-  imports: [HttpClientModule, ApolloModule, HttpLinkModule],
-})
-class AppModule {
-  constructor(apollo: Apollo, httpLink: httpLink) {
-    apollo.create({
-      link: httpLink.create({uri: '/graphql'}),
-      cache: new InMemoryCache(),
-    });
-  }
-}
-
-// a query with apollo-angular
-apollo.query({
-  query: MY_QUERY,
-  context: {
-    // example of setting the headers with context per operation
-    headers: new HttpHeaders().set('X-Custom-Header', 'custom-value'),
-  },
-});
-```
-
-### Middleware
-
-```ts
-import {ApolloLink} from 'apollo-link';
-import {HttpLink} from 'apollo-angular-link-http';
-
-class AppModule {
-  constructor(httpLink: HttpLink) {
-    const http = httpLink.create({uri: '/graphql'});
-    const middleware = new ApolloLink((operation, forward) => {
-      operation.setContext({
-        headers: new HttpHeaders().set(
-          'Authorization',
-          localStorage.getItem('token') || null,
-        ),
-      });
-      return forward(operation);
-    });
-
-    const link = middleware.concat(http);
-  }
-}
-```
-
-### Afterware (error)
-
-```js
-import {ApolloLink} from 'apollo-link';
-import {HttpLink} from 'apollo-angular-link-http';
-import {onError} from 'apollo-link-error';
-
-import {Auth} from './auth.service';
-
-class AppModule {
-  constructor(httpLink: HttpLink, auth: Auth) {
-    const http = httpLink.create({uri: '/graphql'});
-    const error = onError(({networkError}) => {
-      if (networkError.status === 401) {
-        auth.logout();
-      }
-    });
-
-    const link = error.concat(http);
-  }
-}
-```
+Works in the same way as in `apollo-angular-link-http`.
--- packages/apollo-angular-link-http-batch/src/HttpBatchLink.ts
+++ packages/apollo-angular-link-http-batch/src/HttpBatchLink.ts
@@ -1,73 +1,54 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {
-  ApolloLink,
-  Observable as LinkObservable,
-  Operation,
-  RequestHandler,
-} from 'apollo-link';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {ApolloLink, Observable as LinkObservable, Operation} from 'apollo-link';
+import {BatchLink, BatchHandler} from 'apollo-link-batch';
 import {print} from 'graphql/language/printer';
 import {ExecutionResult} from 'graphql';
 import {
   fetch,
-  Options,
-  Request,
+  Body,
   Context,
+  Request,
+  Options,
   mergeHeaders,
   prioritize,
 } from 'apollo-angular-link-http-common';
 
-export class HttpBatchLinkHandler extends ApolloLink {
-  public requester: RequestHandler;
+import {BatchOptions} from './types';
 
-  constructor(private httpClient: HttpClient, private options: Options) {
+const defaults = {
+  batchInterval: 10,
+  batchMax: 10,
+  uri: 'graphql',
+  method: 'POST',
+};
+
+export class HttpBatchLinkHandler extends ApolloLink {
+  public batcher: ApolloLink;
+  private batchInterval: number;
+  private batchMax: number;
+
+  constructor(private httpClient: HttpClient, private options: BatchOptions) {
     super();
 
-    this.requester = (operation: Operation) =>
-      new LinkObservable((observer: any) => {
-        const context: Context = operation.getContext();
+    this.batchInterval = options.batchInterval || defaults.batchInterval;
+    this.batchMax = options.batchMax || defaults.batchMax;
 
-        // decides which value to pick, Context, Options or to just use the default
-        const pick = <K extends keyof Context | keyof Options>(
-          key: K,
-          init?: Context[K] | Options[K],
-        ): Context[K] | Options[K] => {
-          return prioritize(context[key], this.options[key], init);
-        };
-
-        const includeQuery = pick('includeQuery', true);
-        const includeExtensions = pick('includeExtensions', false);
-        const method = pick('method', 'POST');
-        const url = pick('uri', 'graphql');
-        const withCredentials = pick('withCredentials');
+    const batchHandler: BatchHandler = (operations: Operation[]) => {
+      return new LinkObservable((observer: any) => {
+        const body = this.createBody(operations);
+        const headers = this.createHeaders(operations);
+        const {method, uri, withCredentials} = this.createOptions(operations);
 
         const req: Request = {
           method,
-          url,
-          body: {
-            operationName: operation.operationName,
-            variables: operation.variables,
-          },
+          url: uri,
+          body: body,
           options: {
             withCredentials,
-            headers: this.options.headers,
+            headers,
           },
         };
-
-        if (includeExtensions) {
-          req.body.extensions = operation.extensions;
-        }
-
-        if (includeQuery) {
-          req.body.query = print(operation.query);
-        }
-
-        if (context.headers) {
-          req.options.headers = mergeHeaders(
-            req.options.headers,
-            context.headers,
-          );
-        }
 
         const sub = fetch(req, this.httpClient).subscribe({
           next: result => observer.next(result.body),
@@ -81,10 +62,89 @@ export class HttpBatchLinkHandler extends ApolloLink {
           }
         };
       });
+    };
+
+    const batchKey =
+      options.batchKey ||
+      ((operation: Operation) => {
+        return this.createBatchKey(operation);
+      });
+
+    this.batcher = new BatchLink({
+      batchInterval: this.batchInterval,
+      batchMax: this.batchMax,
+      batchKey,
+      batchHandler,
+    });
+  }
+
+  private createOptions(operations: Operation[]): Options {
+    const context: Context = operations[0].getContext();
+
+    return {
+      method: prioritize(context.method, this.options.method, defaults.method),
+      uri: prioritize(context.uri, this.options.uri, defaults.uri),
+      withCredentials: prioritize(
+        context.withCredentials,
+        this.options.withCredentials,
+      ),
+    };
+  }
+
+  private createBody(operations: Operation[]): Body[] {
+    return operations.map(operation => {
+      const includeExtensions = prioritize(
+        operation.getContext().includeExtensions,
+        this.options.includeExtensions,
+        false,
+      );
+      const includeQuery = prioritize(
+        operation.getContext().includeQuery,
+        this.options.includeQuery,
+        true,
+      );
+
+      const body: Body = {
+        operationName: operation.operationName,
+        variables: operation.variables,
+      };
+
+      if (includeExtensions) {
+        body.extensions = operation.extensions;
+      }
+
+      if (includeQuery) {
+        body.query = print(operation.query);
+      }
+
+      return body;
+    });
+  }
+
+  private createHeaders(operations: Operation[]): HttpHeaders {
+    return operations.reduce((headers: HttpHeaders, operation: Operation) => {
+      return mergeHeaders(headers, operation.getContext().headers);
+    }, this.options.headers);
+  }
+
+  private createBatchKey(operation: Operation): string {
+    const context: Context = operation.getContext();
+
+    const headers =
+      context.headers &&
+      context.headers.keys().map((k: string) => context.headers.get(k));
+
+    const opts = JSON.stringify({
+      includeQuery: context.includeQuery,
+      includeExtensions: context.includeExtensions,
+      headers,
+    });
+
+    return prioritize(context.uri, this.options.uri) + opts;
   }
 
   public request(op: Operation): LinkObservable<ExecutionResult> | null {
-    return this.requester(op);
+    return this.batcher.request(op);
   }
 }
 
@@ -92,7 +152,7 @@ export class HttpBatchLinkHandler extends ApolloLink {
 export class HttpBatchLink {
   constructor(private httpClient: HttpClient) {}
 
-  public create(options: Options): HttpBatchLinkHandler {
+  public create(options: BatchOptions): HttpBatchLinkHandler {
     return new HttpBatchLinkHandler(this.httpClient, options);
   }
 }
