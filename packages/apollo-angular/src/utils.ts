ecbe1634273a997ff1583d646fd5facb7317896f
Kamil Kisiela <kamil.kisiela@gmail.com>
2018-05-09 15:18:54 +0200

    refactor: add a small todo

--- packages/apollo-angular/src/utils.ts
+++ packages/apollo-angular/src/utils.ts
@@ -47,6 +47,7 @@ export class ZoneScheduler implements SchedulerLike {
 }
 
 // XXX: Apollo's QueryObservable is not compatible with RxJS
+// TODO: remove it in one of future releases
 // https://github.com/ReactiveX/rxjs/blob/9fb0ce9e09c865920cf37915cc675e3b3a75050b/src/internal/util/subscribeTo.ts#L32
 export function fixObservable<T>(
   obs: ObservableQuery<T> | ApolloObservable<T>,
