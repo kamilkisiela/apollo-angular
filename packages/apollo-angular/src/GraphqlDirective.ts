a12e1b3edc5a08194dc89eec4860a07fb656caa5
Kamil Kisiela <kamil.kisiela@gmail.com>
2018-05-22 23:44:41 +0200

    feat(core): add query options as inputs

--- packages/apollo-angular/src/GraphqlDirective.ts
+++ packages/apollo-angular/src/GraphqlDirective.ts
@@ -6,19 +6,42 @@ import {
   OnInit,
   OnDestroy,
   OnChanges,
+  SimpleChanges,
 } from '@angular/core';
 import {Subject} from 'rxjs';
 import {takeUntil, debounceTime} from 'rxjs/operators';
-import {ApolloQueryResult} from 'apollo-client';
+import {
+  ApolloQueryResult,
+  FetchPolicy,
+  ErrorPolicy,
+  NetworkStatus,
+  ApolloError,
+} from 'apollo-client';
 
 import {Query} from './Query';
 import {QueryRef} from './QueryRef';
 
-export interface GraphqlContext<T> {
-  query: QueryRef<T>;
+export interface GraphqlContext<T, V = Record<string, any>> {
+  query: QueryRef<T, V>;
   result: ApolloQueryResult<T>;
   loading: boolean;
+  networkStatus: NetworkStatus;
+  errors: ApolloError;
   data: T;
+  load: () => void;
+}
+
+function compact(obj: any) {
+  return Object.keys(obj).reduce(
+    (acc, key) => {
+      if (obj[key] !== undefined) {
+        acc[key] = obj[key];
+      }
+
+      return acc;
+    },
+    {} as any,
+  );
 }
 
 @Directive({
@@ -26,7 +49,13 @@ export interface GraphqlContext<T> {
 })
 export class GraphqlDirective<T> implements OnInit, OnDestroy, OnChanges {
   @Input() public graphql: Query<T>;
-  @Input() public variables: Record<any, string>;
+  @Input() public variables?: Record<any, string>;
+  @Input() public pollInterval?: number;
+  @Input() public notifyOnNetworkStatusChange?: number;
+  @Input() public fetchPolicy?: FetchPolicy;
+  @Input() public errorPolicy?: ErrorPolicy;
+  @Input() public displayName?: string;
+  @Input() public context?: Record<string, any> = {};
   @Input() public debounce: number = 0;
 
   private ngDestroy: Subject<void>;
@@ -39,32 +68,76 @@ export class GraphqlDirective<T> implements OnInit, OnDestroy, OnChanges {
 
   public ngOnInit() {
     this.ngDestroy = new Subject<void>();
-    this.ref = this.graphql.query(this.variables);
+    this.initQuery();
+  }
+
+  public ngOnChanges(changes: SimpleChanges) {
+    if ('graphql' in changes) {
+      const {graphql} = changes;
+
+      if (
+        graphql.isFirstChange() ||
+        typeof graphql.previousValue !== 'function'
+      ) {
+        return;
+      }
+
+      this.ngDestroy.next();
+      this.initQuery();
+    }
+  }
+
+  private initQuery() {
+    this.ref = this.graphql.query(this.variables, this.createOptions());
 
     this.createView({
       loading: true,
-    });
+    } as any);
 
     this.ref.valueChanges
       .pipe(takeUntil(this.ngDestroy), debounceTime(this.debounce))
-      .subscribe(result => this.updateView(result));
+      .subscribe(result => this.handleResult(result));
   }
 
-  public ngOnChanges() {
-    // TODO: handle that
+  private handleResult(result: ApolloQueryResult<T>) {
+    this.updateView(result);
   }
 
   private createView(result: any) {
     this.vcRef.createEmbeddedView(this.tRef, {
       query: this.ref,
       loading: result.loading,
+      errors:
+        result.errors && result.errors.length > 0
+          ? new ApolloError({graphQLErrors: result.errors})
+          : null,
       data: result.data,
+      networkStatus: result.networkStatus,
       result,
     });
   }
 
-  private updateView(result: any) {
+  private updateView(result: ApolloQueryResult<T>) {
     this.createView(result);
+  }
+
+  private createOptions() {
+    return compact({
+      pollInterval: this.pollInterval,
+      fetchPolicy: this.fetchPolicy,
+      errorPolicy: this.errorPolicy,
+      notifyOnNetworkStatusChange: this.notifyOnNetworkStatusChange,
+      metadata: {
+        // TODO: make the structure of a component framework agnostic
+        component: {
+          displayName: this.displayName,
+        },
+        reactComponent: {
+          displayName: this.displayName,
+        },
+      },
+      context: this.context,
+    });
   }
 
   public ngOnDestroy() {
