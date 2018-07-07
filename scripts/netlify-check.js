a321b99596c182a1d098924169b4021283ba0c91
Kamil Kisiela <kamil.kisiela@gmail.com>
2018-07-07 13:43:29 +0200

    ci: netlify

--- scripts/netlify-check.js
+++ scripts/netlify-check.js
@@ -1,8 +1,6 @@
 #!/usr/bin/env node
 const shell = require('shelljs');
 
-console.log('env', process.env.NODE_ENV);
-
 if (process.env.NODE_ENV !== 'development') {
   shell.exec(process.argv[2]);
 } else {
