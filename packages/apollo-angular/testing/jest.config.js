82b64a5f8a4e49fb85c6c06273989a073f551f24
Kamil Kisiela <kamil.kisiela@gmail.com>
2018-07-07 11:26:34 +0200

    build: yarn workspaces

--- packages/apollo-angular/testing/jest.config.js
+++ packages/apollo-angular/testing/jest.config.js
@@ -2,7 +2,7 @@ const jestConfig = require('../../../jest.config');
 
 module.exports = Object.assign(jestConfig(), {
   transform: {
-    '.ts': '<rootDir>/../node_modules/ts-jest/preprocessor.js',
+    '.ts': '<rootDir>/../../../node_modules/ts-jest/preprocessor.js',
   },
   moduleNameMapper: {
     'apollo-angular': '<rootDir>/../',
