535b26b138b3226c6b4f3e308eae732427fe1979
Kamil Kisiela <kamil.kisiela@gmail.com>
2018-08-17 10:37:45 +0200

    fix(boost): init via token does not work if ApolloBoost is not used

--- packages/apollo-angular-boost/src/ApolloBoostModule.ts
+++ packages/apollo-angular-boost/src/ApolloBoostModule.ts
@@ -1,11 +1,24 @@
-import {NgModule} from '@angular/core';
+import {NgModule, Optional, Inject} from '@angular/core';
 import {ApolloModule} from 'apollo-angular';
 import {HttpLinkModule} from 'apollo-angular-link-http';
 
 import {ApolloBoost} from './ApolloBoost';
+import {APOLLO_BOOST_CONFIG} from './tokens';
+import {PresetConfig} from './types';
 
 @NgModule({
   imports: [ApolloModule, HttpLinkModule],
   providers: [ApolloBoost],
 })
-export class ApolloBoostModule {}
+export class ApolloBoostModule {
+  constructor(
+    boost: ApolloBoost,
+    @Optional()
+    @Inject(APOLLO_BOOST_CONFIG)
+    config?: PresetConfig,
+  ) {
+    if (config) {
+      boost.create(config);
+    }
+  }
+}
