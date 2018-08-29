c20c2e26ddf2e5e11aab3dc7d8807faeeb53004b
Kamil Kisiela <kamil.kisiela@gmail.com>
2018-08-29 21:07:02 +0200

    refactor(core): rollback

--- packages/apollo-angular/testing/jest.config.js
+++ packages/apollo-angular/testing/jest.config.js
@@ -1,11 +1,6 @@
 const jestConfig = require('../../../jest.config');
 
 module.exports = Object.assign(jestConfig(), {
-  globals: {
-    'ts-jest': {
-      tsConfigFile: 'tsconfig.test.json',
-    },
-  },
   transform: {
     '.ts': 'ts-jest',
   },
