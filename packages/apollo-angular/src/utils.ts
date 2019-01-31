3c07a86d77073f6dd41c69066d02b60a102abb40
Kamil Kisiela <kamil.kisiela@gmail.com>
2019-01-31 12:22:03 +0100

    Fix generic type

--- packages/apollo-angular/src/utils.ts
+++ packages/apollo-angular/src/utils.ts
@@ -50,9 +50,7 @@ export class ZoneScheduler implements SchedulerLike {
 // XXX: Apollo's QueryObservable is not compatible with RxJS
 // TODO: remove it in one of future releases
 // https://github.com/ReactiveX/rxjs/blob/9fb0ce9e09c865920cf37915cc675e3b3a75050b/src/internal/util/subscribeTo.ts#L32
-export function fixObservable<T>(
-  obs: ObservableQuery<T> | ApolloObservable<T>,
-): ObservableQuery<T> | ApolloObservable<T> {
+export function fixObservable<T>(obs: T): T {
   (obs as any)[observable] = () => obs;
   return obs;
 }
