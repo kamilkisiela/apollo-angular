fdebf105d465c20a9d9c27014711bbc815e6df31
Arda TANRIKULU <ardatanrikulu@gmail.com>
2019-07-01 02:13:42 -0400

    Add bundle-size check

---
+++ bundle-test/src/index.js
@@ -0,0 +1,18 @@
+import * as ApolloAngular from 'apollo-angular';
+import * as ApolloAngularBoost from 'apollo-angular-boost';
+import * as ApolloAngularLinkHeaders from 'apollo-angular-link-headers';
+import * as ApolloAngularLinkHttp from 'apollo-angular-link-http';
+import * as ApolloAngularLinkHttpBatch from 'apollo-angular-link-http-batch';
+import * as ApolloAngularLinkHttpCommon from 'apollo-angular-link-http-common';
+import * as ApolloAngularLinkPersisted from 'apollo-angular-link-persisted';
+
+console.log(
+  ApolloAngular,
+  ApolloAngularBoost,
+  ApolloAngularLinkHeaders,
+  ApolloAngularLinkHttp,
+  ApolloAngularLinkHttpBatch,
+  ApolloAngularLinkHttpCommon,
+  ApolloAngularLinkPersisted,
+);
+
