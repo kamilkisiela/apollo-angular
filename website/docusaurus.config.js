f8b16bb5ddbc4d1d52e441e8bd0d2b966ac0fcb0
Kamil Kisiela <kamil.kisiela@gmail.com>
2020-08-03 12:54:22 +0200

    New Home (#1536)

---
+++ website/docusaurus.config.js
@@ -0,0 +1,154 @@
+module.exports = {
+  title: 'Apollo Angular',
+  tagline:
+    'A guide to using the Apollo GraphQL Client with Angular',
+
+  url: 'https://apollo-angular.com',
+  baseUrl: '/',
+  favicon: 'img/favicon/favicon.png',
+
+  organizationName: 'kamilkisiela',
+  projectName: 'apollo-angular',
+
+  themeConfig: {
+    colorMode: {
+      disableSwitch: true
+    },
+    sidebarCollapsible: true,
+    image: 'img/github/app-action.jpg',
+    navbar: {
+      title: 'Apollo Angular',
+      logo: {
+        alt: 'Apollo Angular logo',
+        src: 'img/logo/logo-apollo-space.svg',
+      }
+    },
+    footer: {
+      style: 'dark',
+      copyright: `Copyright © ${new Date().getFullYear()} Kamil Kisiela. All rights reserved.`,
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Introduction',
+              to: 'docs/index',
+            },
+            {
+              label: 'Basics',
+              to: 'docs/basics/setup',
+            },
+            {
+              label: 'Features',
+              to: 'docs/features/error-handling',
+            },
+            {
+              label: 'Guides',
+              to: 'docs/guides/state-management',
+            },
+            {
+              label: 'Recipes',
+              to: 'docs/recipes/query-splitting',
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Discord',
+              href: 'https://discord.gg/xud7bH9',
+            },
+            {
+              label: 'Other projects',
+              href: 'https://the-guild.dev/open-source',
+            },
+            {
+              label: 'About us',
+              href: 'https://the-guild.dev',
+            },
+            {
+              label: 'Community Meetings',
+              href: 'https://github.com/the-guild-org/community-meetings',
+            },
+          ],
+        },
+        {
+          title: 'Social',
+          items: [
+            {
+              label: 'Blog',
+              href: 'https://the-guild.dev/blog',
+            },
+            {
+              label: 'GitHub',
+              href: 'https://github.com/kamilkisiela/apollo-angular',
+            },
+            {
+              label: 'Twitter',
+              href: 'https://twitter.com/kamilkisiela',
+            },
+            {
+              label: 'LinkedIn',
+              href: 'https://www.linkedin.com/company/the-guild-software',
+            },
+          ],
+        },
+      ],
+    },
+    googleAnalytics: {
+      trackingID: 'UA-125180910-5',
+    },
+    // algolia: {
+    //   apiKey: 'c81d6a17b6d40971f230c0d79b03ff23',
+    //   indexName: 'apollo-angular',
+    //   algoliaOptions: {},
+    // },
+  },
+  scripts: [
+    {
+      src: '/js/scroll-to.js',
+    },
+    {
+      src: 'https://the-guild.dev/static/banner.js',
+      async: true,
+      defer: true,
+    },
+  ],
+  stylesheets: [
+    'https://fonts.googleapis.com/css?family=Lato:300,400,700,900&display=swap',
+  ],
+  presets: [
+    [
+      require.resolve('@docusaurus/preset-classic'),
+      {
+        docs: {
+          path: 'docs',
+          include: ['**/*.md', '**/*.mdx'],
+          sidebarPath: require.resolve('./sidebars.js'),
+          editUrl:
+            'https://github.com/kamilkisiela/apollo-angular/edit/master/website/',
+        },
+        // theme: {
+        //   customCss: require.resolve('./src/css/custom.css'),
+        // },
+        sitemap: {
+          cacheTime: 600 * 1001, // 600 sec - cache purge period
+          changefreq: 'weekly',
+          priority: 0.5,
+        },
+      },
+    ],
+  ],
+  plugins: [
+    [
+      require.resolve('@docusaurus/plugin-ideal-image'),
+      {
+        size: 800,
+        max: 800,
+        min: 200,
+        quality: 100,
+      },
+    ],
+  ],
+};
