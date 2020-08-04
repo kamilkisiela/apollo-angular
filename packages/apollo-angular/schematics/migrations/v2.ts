009957670f0907f12d474469289c5a63552b9f5e
Kamil Kisiela <kamil.kisiela@gmail.com>
2020-08-04 15:58:30 +0200

    ng-update - Migration to v2

---
+++ packages/apollo-angular/schematics/migrations/v2.ts
@@ -0,0 +1,347 @@
+import {
+  Rule,
+  Tree,
+  chain,
+  SchematicContext,
+  UpdateRecorder,
+} from '@angular-devkit/schematics';
+import {NodePackageInstallTask} from '@angular-devkit/schematics/tasks';
+import * as ts from 'typescript';
+import {getJsonFile} from '../utils';
+import {dependenciesMap} from '../install/index';
+
+export default function (): Rule {
+  return chain([migrateImports, migrateTsConfig, migrateDependencies]);
+}
+
+function migrateDependencies() {
+  return (tree: Tree, context: SchematicContext) => {
+    const packageJsonPath = 'package.json';
+    const packageJson = getJsonFile(tree, packageJsonPath);
+
+    packageJson.dependencies = packageJson.dependencies || {};
+
+    for (const dependency in dependenciesMap) {
+      if (dependenciesMap.hasOwnProperty(dependency)) {
+        const version = dependenciesMap[dependency];
+
+        packageJson.dependencies[dependency] = version;
+      }
+    }
+
+    const packagesToRemove = [
+      'graphql-tag',
+      'apollo-client',
+      'apollo-cache',
+      'apollo-cache-inmemory',
+      'apollo-utilities',
+      'apollo-link',
+      'apollo-link-http',
+      'apollo-link-batch-http',
+      'apollo-link-context',
+      'apollo-link-error',
+      'apollo-link-schema',
+      'apollo-link-ws',
+      'apollo-angular-link-http',
+      'apollo-angular-link-http-batch',
+      'apollo-angular-link-headers'
+    ];
+
+    const removedPackages: string[] = [];
+
+    packagesToRemove.forEach((packageName) => {
+      let removed = false;
+
+      if (packageJson.dependencies?.[packageName]) {
+        delete packageJson.dependencies[packageName];
+        removed = true;
+      }
+
+      if (packageJson.devDependencies?.[packageName]) {
+        delete packageJson.devDependencies[packageName];
+        removed = true;
+      }
+
+      if (removed) {
+        removedPackages.push(packageName);
+      }
+    });
+
+    removedPackages.forEach((packageName) => {
+      context.logger.info(`Removed ${packageName} dependency`);
+    });
+
+    // save the changed file
+    tree.overwrite(packageJsonPath, JSON.stringify(packageJson, null, 2));
+
+    // schedule `npm install`
+    context.addTask(new NodePackageInstallTask());
+
+    return tree;
+  };
+}
+
+export async function migrateTsConfig(tree: Tree) {
+  const tsconfigPath = 'tsconfig.json';
+  const tsconfig = getJsonFile(tree, tsconfigPath);
+  const compilerOptions: ts.CompilerOptions = tsconfig.compilerOptions;
+
+  if (compilerOptions) {
+    compilerOptions.allowSyntheticDefaultImports = true;
+    tree.overwrite(tsconfigPath, JSON.stringify(tsconfig, null, 2));
+  } else {
+    const tsconfigBasePath = 'tsconfig.base.json';
+    const tsconfigBase = getJsonFile(tree, tsconfigBasePath);
+    const baseCompilerOptions: ts.CompilerOptions =
+      tsconfigBase.compilerOptions;
+
+    if (baseCompilerOptions) {
+      baseCompilerOptions.allowSyntheticDefaultImports = true;
+      tree.overwrite(tsconfigBasePath, JSON.stringify(tsconfigBase, null, 2));
+    }
+  }
+}
+
+function getIdentifiers(
+  namedBindings: ts.NamedImportBindings,
+  onIdentifier: (event: {name: string; alias?: string}) => void,
+) {
+  namedBindings.forEachChild((named) => {
+    if (ts.isImportSpecifier(named)) {
+      const name =
+        named.propertyName && typeof named.propertyName !== 'undefined'
+          ? named.propertyName.escapedText.toString()
+          : named.name.escapedText.toString();
+
+      onIdentifier({
+        name,
+        alias: name === named.name.escapedText.toString() ? undefined : name,
+      });
+    }
+  });
+}
+
+export async function migrateImports(tree: Tree) {
+  const importsMap: Record<
+    string,
+    Array<{
+      name: string;
+      alias?: string;
+    }>
+  > = {};
+
+  function collectIdentifiers(
+    packageName: string,
+    namedBindings: ts.NamedImportBindings,
+  ) {
+    getIdentifiers(namedBindings, ({name, alias}) => {
+      if (!importsMap[packageName]) {
+        importsMap[packageName] = [];
+      }
+
+      importsMap[packageName].push({
+        name,
+        alias,
+      });
+    });
+  }
+
+  function redirectImport({
+    source,
+    target,
+    modulePath,
+    statement,
+    recorder,
+  }: {
+    source: string;
+    target: string;
+    modulePath: string;
+    statement: any;
+    recorder: UpdateRecorder;
+  }) {
+    if (modulePath === source) {
+      if (statement.importClause.namedBindings) {
+        collectIdentifiers(target, statement.importClause.namedBindings);
+      }
+
+      recorder.remove(statement.getStart(), statement.getWidth());
+    }
+  }
+
+  tree.visit((path) => {
+    if (path.includes('node_modules') || !path.endsWith('.ts')) {
+      return;
+    }
+
+    const sourceFile = ts.createSourceFile(
+      path,
+      tree.read(path).toString(),
+      ts.ScriptTarget.Latest,
+      true,
+    );
+
+    const recorder = tree.beginUpdate(path);
+
+    sourceFile.statements.forEach((statement) => {
+      if (
+        ts.isImportDeclaration(statement) &&
+        ts.isStringLiteral(statement.moduleSpecifier)
+      ) {
+        const nodeText = statement.moduleSpecifier.getText(sourceFile);
+        const modulePath = statement.moduleSpecifier
+          .getText(sourceFile)
+          .substr(1, nodeText.length - 2);
+
+        redirectImport({
+          source: 'apollo-cache-inmemory',
+          target: '@apollo/client/core',
+          recorder,
+          statement,
+          modulePath,
+        });
+
+        redirectImport({
+          source: 'apollo-client',
+          target: '@apollo/client/core',
+          recorder,
+          statement,
+          modulePath,
+        });
+
+        redirectImport({
+          source: 'apollo-link',
+          target: '@apollo/client/core',
+          recorder,
+          statement,
+          modulePath,
+        });
+
+        redirectImport({
+          source: 'apollo-cache',
+          target: '@apollo/client/core',
+          recorder,
+          statement,
+          modulePath,
+        });
+
+        redirectImport({
+          source: 'apollo-angular-link-http',
+          target: 'apollo-angular/http',
+          recorder,
+          statement,
+          modulePath,
+        });
+
+        redirectImport({
+          source: 'apollo-angular-link-http-batch',
+          target: 'apollo-angular/http',
+          recorder,
+          statement,
+          modulePath,
+        });
+
+        redirectImport({
+          source: 'apollo-utilities',
+          target: '@apollo/client/utilities',
+          recorder,
+          statement,
+          modulePath,
+        });
+
+        redirectImport({
+          source: 'apollo-link-http',
+          target: '@apollo/client/link/http',
+          recorder,
+          statement,
+          modulePath,
+        });
+
+        redirectImport({
+          source: 'apollo-link-batch-http',
+          target: '@apollo/client/link/batch-http',
+          recorder,
+          statement,
+          modulePath,
+        });
+
+        redirectImport({
+          source: 'apollo-link-context',
+          target: '@apollo/client/link/context',
+          recorder,
+          statement,
+          modulePath,
+        });
+
+        redirectImport({
+          source: 'apollo-link-error',
+          target: '@apollo/client/link/error',
+          recorder,
+          statement,
+          modulePath,
+        });
+
+        redirectImport({
+          source: 'apollo-link-schema',
+          target: '@apollo/client/link/schema',
+          recorder,
+          statement,
+          modulePath,
+        });
+
+        redirectImport({
+          source: 'apollo-link-ws',
+          target: '@apollo/client/link/ws',
+          recorder,
+          statement,
+          modulePath,
+        });
+
+        redirectImport({
+          source: 'apollo-angular',
+          target: 'apollo-angular',
+          recorder,
+          statement,
+          modulePath,
+        });
+
+        redirectImport({
+          source: 'apollo-angular-link-headers',
+          target: 'apollo-angular/headers',
+          recorder,
+          statement,
+          modulePath,
+        });
+
+        if (modulePath === 'graphql-tag') {
+          if (!importsMap['apollo-angular']) {
+            importsMap['apollo-angular'] = [];
+          }
+          importsMap['apollo-angular'].push({
+            name: 'gql',
+            alias:
+              statement.importClause.name.escapedText.toString() === 'gql'
+                ? undefined
+                : statement.importClause.name.escapedText.toString(),
+          });
+
+          recorder.remove(statement.getStart(), statement.getWidth());
+        }
+      }
+    });
+
+    const importSources = Object.keys(importsMap);
+    importSources.forEach((importSource) => {
+      const props = importsMap[importSource]
+        .map((im) => (im.alias ? `${im.name} as ${im.alias}` : im.name))
+        .join(', ');
+      recorder.insertLeft(
+        sourceFile.getStart(),
+        `import {${props}} from '${importSource}';\n`,
+      );
+    });
+
+    if (importSources.length) {
+      tree.commitUpdate(recorder);
+    }
+  });
+}
---
+++ packages/apollo-angular/schematics/tests/migration-v2.spec.ts
@@ -0,0 +1,134 @@
+import {resolve} from 'path';
+import {Tree} from '@angular-devkit/schematics';
+import {SchematicTestRunner} from '@angular-devkit/schematics/testing';
+import {getFileContent} from '@schematics/angular/utility/test';
+import {CompilerOptions} from 'typescript';
+
+import {createTestApp, parseJSON} from '../utils';
+import {dependenciesMap} from '../install';
+
+const migrationsPath = resolve(__dirname, '../migrations.json');
+const migrationName = 'migration-2.0.0';
+
+describe('Migration: Apollo Angular V2', () => {
+  let runner: SchematicTestRunner;
+  let appTree: Tree;
+
+  beforeEach(async () => {
+    appTree = await createTestApp();
+    runner = new SchematicTestRunner('schematics', migrationsPath);
+  });
+
+  test('should update imports', async () => {
+    appTree.create(
+      'file.ts',
+      `
+        import { InMemoryCache } from 'apollo-cache-inmemory';
+        import { ApolloClient } from 'apollo-client';
+        import { ApolloLink } from 'apollo-link';
+        import { onError } from 'apollo-link-error';
+        import ApolloClient from 'apollo-client';
+        import { HttpLink } from 'apollo-angular-link-http';
+        import { Apollo } from 'apollo-angular';
+        import gql from 'graphql-tag';
+      `,
+    );
+    const tree = await runner
+      .runSchematicAsync(migrationName, {}, appTree)
+      .toPromise();
+    const file = tree.readContent('file.ts').trim();
+
+    expect(file).toContain(
+      `import {InMemoryCache, ApolloClient, ApolloLink} from '@apollo/client/core';`,
+    );
+    expect(file).toContain(
+      `import {onError} from '@apollo/client/link/error';`,
+    );
+    expect(file).toContain(`import {HttpLink} from 'apollo-angular/http';`);
+    expect(file).toContain(`import {Apollo, gql} from 'apollo-angular';`);
+  });
+
+  test('should update imports (with aliases)', async () => {
+    appTree.create(
+      'file.ts',
+      `
+        import { InMemoryCache } from 'apollo-cache-inmemory';
+        import { ApolloClient } from 'apollo-client';
+        import { ApolloLink } from 'apollo-link';
+        import { onError } from 'apollo-link-error';
+        import ApolloClient from 'apollo-client';
+        import { HttpLink } from 'apollo-angular-link-http';
+        import { Apollo } from 'apollo-angular';
+        import graphql from 'graphql-tag';
+      `,
+    );
+    const tree = await runner
+      .runSchematicAsync(migrationName, {}, appTree)
+      .toPromise();
+
+    const file = tree.readContent('file.ts').trim();
+
+    expect(file).toContain(
+      `import {InMemoryCache, ApolloClient, ApolloLink} from '@apollo/client/core';`,
+    );
+    expect(file).toContain(
+      `import {onError} from '@apollo/client/link/error';`,
+    );
+    expect(file).toContain(`import {HttpLink} from 'apollo-angular/http';`);
+    expect(file).toContain(
+      `import {Apollo, gql as graphql} from 'apollo-angular';`,
+    );
+  });
+
+  test('should enable allowSyntheticDefaultImports in tsconfig.base.json', async () => {
+    const tree = await runner
+      .runSchematicAsync(migrationName, {}, appTree)
+      .toPromise();
+    const rootModulePath = '/tsconfig.base.json';
+    const compilerOptions: CompilerOptions = parseJSON(
+      getFileContent(tree, rootModulePath),
+    ).compilerOptions;
+
+    expect(compilerOptions.allowSyntheticDefaultImports).toEqual(true);
+  });
+
+  test('should update package.json', async () => {
+    const oldPackageJson = parseJSON(
+      appTree.read('package.json').toString('utf-8'),
+    );
+
+    oldPackageJson.dependencies = {
+      ...oldPackageJson.dependencies,
+      'apollo-angular': '^1.8.0',
+      'apollo-angular-link-http': '^1.9.0',
+      'apollo-link': '^1.2.11',
+      'apollo-client': '^2.6.0',
+      'apollo-cache-inmemory': '^1.6.0',
+      'graphql-tag': '^2.10.0',
+      graphql: '^14.5.0',
+    };
+
+    appTree.overwrite('package.json', JSON.stringify(oldPackageJson, null, 2));
+
+    const tree = await runner
+      .runSchematicAsync(migrationName, {}, appTree)
+      .toPromise();
+
+    const packageJson = parseJSON(tree.readContent('package.json'));
+
+    expect(packageJson.dependencies['apollo-angular']).toEqual(
+      dependenciesMap['apollo-angular'],
+    );
+    expect(packageJson.dependencies['@apollo/client']).toEqual(
+      dependenciesMap['@apollo/client'],
+    );
+
+    expect(
+      packageJson.dependencies['apollo-angular-link-http'],
+    ).toBeUndefined();
+    expect(packageJson.dependencies['apollo-client']).toBeUndefined();
+    expect(packageJson.dependencies['apollo-link']).toBeUndefined();
+    expect(packageJson.dependencies['apollo-cache-inmemory']).toBeUndefined();
+    expect(packageJson.dependencies['graphql-tag']).toBeUndefined();
+  });
+});
