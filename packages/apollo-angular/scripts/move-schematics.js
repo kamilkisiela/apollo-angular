c8d94f5fa229d92e9af0c6184c5cd30e31e6bcaf
Kamil Kisiela <kamil.kisiela@gmail.com>
2020-08-04 20:16:33 +0200

    Release v2.0.0

--- packages/apollo-angular/scripts/move-schematics.js
+++ packages/apollo-angular/scripts/move-schematics.js
@@ -3,5 +3,6 @@ const cpx = require('cpx');
 cpx.copySync("schematics/install/files/**/*", "build/schematics/install/files");
 cpx.copySync('schematics/README.md', "build/schematics");
 cpx.copySync('schematics/collection.json', "build/schematics");
+cpx.copySync('schematics/migrations.json', "build/schematics");
 cpx.copySync('schematics/install/schema.json', "build/schematics/install");
 
