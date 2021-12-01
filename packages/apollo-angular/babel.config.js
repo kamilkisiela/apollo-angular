1181d1494a2ee81a7e3c2db48ca8bd4d706b9401
Joseph Quinn <423821+quinnjr@users.noreply.github.com>
2021-12-01 08:34:01 -0500

    Updating with Angular 13 support (#1729)
    * Updating with Angular 13 support
    
    - Updating dependencies to support Angular v13.
    - Updating Jest.
    - Updating Jest testing to work with Angular v13.
    - Changing module output in root `tsconfig.json` to produce ES6 code.
    
    NOTE:
    I had to skip test 'should run inside Zone' at line 588 in
    [packages/apollo-angular/tests/Apollo.spec.ts](packages/apollo-angular/tests/Apollo.spec.ts)
    since `rxjs` no longer gives information on operator schedulers.
    
    * Adding testing workflow for Angular 13
    
    * Removing `umdModuleIds` from `ng-package` configurations.
    
    - `ng-packagr` no longer supports building UMD modules as of v13.0.0.
      See
      [CHANGELOG](https://github.com/ng-packagr/ng-packagr/blob/master/CHANGELOG.md)
    
    * Correcting module exports in projects `package.json`s
    
    * Fixing issues running test suite
    
    * Test last 3 versions of Angular
    
    * Update .github/workflows/main.yml
    
    * Update packages/apollo-angular/package.json
    
    Co-authored-by: Kamil Kisiela <kamil.kisiela@gmail.com>

---
+++ packages/apollo-angular/babel.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          esmodules: true
+        },
+        modules: 'cjs'
+      }
+    ]
+  ]
+}
