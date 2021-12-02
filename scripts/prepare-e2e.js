4d1d8d798c2cd5ed08ba62d2666b9d1af6715c09
Kamil Kisiela <kamil.kisiela@gmail.com>
2021-12-02 13:00:10 +0100

    Run E2E (#1737)

---
+++ scripts/prepare-e2e.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+
+const path = require('path');
+const fs = require('fs');
+
+const cwd = process.cwd();
+const [, , name] = process.argv;
+
+function updateComponent() {
+  let filepath = path.join(cwd, `./${name}/src/app/app.component.ts`);
+  const code =
+    `import { Apollo } from 'apollo-angular';\n` +
+    fs
+      .readFileSync(filepath, 'utf8')
+      .replace(
+        'AppComponent {',
+        'AppComponent { constructor(private apollo: Apollo) {}',
+      );
+
+  fs.writeFileSync(filepath, code, 'utf8');
+}
+
+function updateCypress() {
+  let filepath = path.join(cwd, `./${name}/cypress/integration/spec.ts`);
+  const code = fs
+    .readFileSync(filepath, 'utf8')
+    .replace(`cy.contains('sandbox app is running!')`, '');
+
+  fs.writeFileSync(filepath, code, 'utf8');
+}
+
+updateComponent();
+updateCypress();
