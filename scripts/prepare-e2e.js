353ba23cfd810bc13dafb9870c12415f568f4ff7
Kamil Kisiela <kamil.kisiela@gmail.com>
2021-12-02 11:55:40 +0000

    Fix code update

--- scripts/prepare-e2e.js
+++ scripts/prepare-e2e.js
@@ -8,23 +8,23 @@ const [, , name] = process.argv;
 
 function updateComponent() {
   let filepath = path.join(cwd, `./${name}/src/app/app.component.ts`);
-  let code = fs.readFileSync(filepath, 'utf8');
-
-  code = `import { Apollo } from 'apollo-angular';\n` + code;
-
-  code.replace(
-    'AppComponent {',
-    'AppComponent { constructor(private apollo: Apollo) {}',
-  );
+  const code =
+    `import { Apollo } from 'apollo-angular';\n` +
+    fs
+      .readFileSync(filepath, 'utf8')
+      .replace(
+        'AppComponent {',
+        'AppComponent { constructor(private apollo: Apollo) {}',
+      );
 
   fs.writeFileSync(filepath, code, 'utf8');
 }
 
 function updateCypress() {
   let filepath = path.join(cwd, `./${name}/cypress/integration/spec.ts`);
-  let code = fs.readFileSync(filepath, 'utf8');
-
-  code.replace(`cy.contains('sandbox app is running!')`, '');
+  const code = fs
+    .readFileSync(filepath, 'utf8')
+    .replace(`cy.contains('sandbox app is running!')`, '');
 
   fs.writeFileSync(filepath, code, 'utf8');
 }
