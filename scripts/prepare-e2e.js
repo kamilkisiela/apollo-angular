ef950d4b218f9db699582ecd47acfec50b6dd587
Kamil Kisiela <kamil.kisiela@gmail.com>
2022-02-16 12:34:00 +0100

    assert

--- scripts/prepare-e2e.js
+++ scripts/prepare-e2e.js
@@ -4,18 +4,19 @@ const path = require('path');
 const fs = require('fs');
 
 const cwd = process.cwd();
-const [, , name] = process.argv;
+const [, , name, version] = process.argv;
 
 function updateComponent() {
   let filepath = path.join(cwd, `./${name}/src/app/app.component.ts`);
   const code =
     `import { Apollo } from 'apollo-angular';\n` +
     `import { version } from 'graphql';\n` +
+    `window.GRAPHQL_VERSION = version;\n`
     fs
       .readFileSync(filepath, 'utf8')
       .replace(
         'AppComponent {',
-        'AppComponent { constructor(private apollo: Apollo) { console.info("GraphQL version:", version); }',
+        'AppComponent { constructor(private apollo: Apollo) {}',
       );
 
   fs.writeFileSync(filepath, code, 'utf8');
@@ -25,7 +26,7 @@ function updateCypress() {
   let filepath = path.join(cwd, `./${name}/cypress/integration/spec.ts`);
   const code = fs
     .readFileSync(filepath, 'utf8')
-    .replace(`cy.contains('sandbox app is running!')`, '');
+    .replace(`cy.contains('sandbox app is running!')`, `cy.window().its('GRAPHQL_VERSION').should('equal', '${version}')`);
 
   fs.writeFileSync(filepath, code, 'utf8');
 
