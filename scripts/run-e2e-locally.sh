80e296288910f81ebe11225c201ec75e555348de
roypeled <peled.roy@gmail.com>
2023-11-19 11:18:05 +0200

    fixed e2e test

--- scripts/run-e2e-locally.sh
+++ scripts/run-e2e-locally.sh
@@ -1,5 +1,6 @@
 rm -rf testapp
 ng new testapp --package-manager yarn --defaults --minimal --skip-git
+(cd packages/apollo-angular/build && yarn pack --filename apollo-angular.tgz && mv apollo-angular.tgz ../apollo-angular.tgz)
 (cd testapp && ng add ../packages/apollo-angular/apollo-angular.tgz --graphql '16.0.0' --defaults --verbose --skip-confirmation)
 (cd testapp && yarn ng run testapp:build:production)
 (cd testapp && ng add @cypress/schematic --defaults --verbose --skip-confirmation)
