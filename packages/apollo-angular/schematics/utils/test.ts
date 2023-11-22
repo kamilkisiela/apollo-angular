4c866732ff1de43d7e47b4eed762fbf41d0e04f9
Adrien Crivelli <adrien.crivelli@gmail.com>
2023-11-22 11:21:40 +0900

    Unit tests use hardcoded project name

--- packages/apollo-angular/schematics/utils/test.ts
+++ packages/apollo-angular/schematics/utils/test.ts
@@ -3,7 +3,7 @@ import { SchematicTestRunner, UnitTestTree } from '@angular-devkit/schematics/te
 
 const collectionPath = join(__dirname, '../collection.json');
 
-export async function createTestApp(appOptions = {}): Promise<UnitTestTree> {
+async function createTestApp(appOptions = {}): Promise<UnitTestTree> {
   const runner = new SchematicTestRunner('apollo-angular', collectionPath);
 
   const workspaceTree = await runner
@@ -23,4 +23,13 @@ export async function createTestApp(appOptions = {}): Promise<UnitTestTree> {
       },
       workspaceTree,
     )
+}
+
+export async function runNgAdd(standalone: boolean): Promise<UnitTestTree> {
+  const projectName = 'apollo';
+  const appTree = await createTestApp({ standalone: standalone, name: projectName });
+
+  const runner = new SchematicTestRunner('apollo-angular', collectionPath);
+
+  return await runner.runSchematic('ng-add', { project: projectName }, appTree);
 }
