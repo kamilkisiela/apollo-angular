46b9ebfcc975f47412c2a44220bcdd138299495d
Adrien Crivelli <adrien.crivelli@gmail.com>
2024-09-19 16:11:04 +0200

    Drop comments on website
    Because those are mostly either low quality questions/feedback or stale
    information.

--- website/theme.config.tsx
+++ website/theme.config.tsx
@@ -1,31 +1,10 @@
 /* eslint sort-keys: error */
-import { useRouter } from 'next/router';
-import { defineConfig, Giscus, PRODUCTS, useTheme } from '@theguild/components';
+import { defineConfig, PRODUCTS } from '@theguild/components';
 
 export default defineConfig({
   docsRepositoryBase: 'https://github.com/kamilkisiela/apollo-angular/tree/master/website',
   main({ children }) {
-    const { resolvedTheme } = useTheme();
-    const { route } = useRouter();
-
-    const comments = route !== '/' && (
-      <Giscus
-        // ensure giscus is reloaded when client side route is changed
-        key={route}
-        repo="kamilkisiela/apollo-angular"
-        repoId="MDEwOlJlcG9zaXRvcnk1NjM1NjcwNg=="
-        category="Docs Discussions"
-        categoryId="DIC_kwDOA1vvYs4CSDSL"
-        mapping="pathname"
-        theme={resolvedTheme}
-      />
-    );
-    return (
-      <>
-        {children}
-        {comments}
-      </>
-    );
+    return <>{children}</>;
   },
   websiteName: 'Apollo Angular',
   description: 'A fully-featured GraphQL client for Angular',
